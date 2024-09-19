c1ca65bb52bc80a3149c12b8ca3d0e2e4d675b54
Joe Pietrzyk <joepietrzyk@gmail.com>
2024-09-18 22:03:58 -0400

    Get the button correctly added to old reddit

--- src/content-scripts/reddit.ts
+++ src/content-scripts/reddit.ts
@@ -1,22 +1,42 @@
-﻿import {isBrowser, isElement} from "../common";
+﻿import { isBrowser, isElement } from "../common";
+import { UserPreferences, loadPreferences } from "../settings";
 
 // Run the main logic only when the script is executed directly
 if (isBrowser()) {
-    console.log('Is browser!');
-    main();
+    loadPreferences().then(preferences => main(preferences));
 }
 
 /**
- * main entry point functionality to execute
+ * Interface representing the result of determining whether the page is using new or old Reddit.
  */
-function main(): void {
+interface RedditVersion {
+    /**
+     * Indicates if the current page is using new Reddit.
+     */
+    isNewReddit: boolean;
+
+    /**
+     * Indicates if the current page is using old Reddit.
+     */
+    isOldReddit: boolean;
+}
+
+/**
+ * Determines if the current page is using new or old Reddit.
+ * @returns An object indicating whether the page is new or old Reddit.
+ */
+function isNewOrOldReddit(): RedditVersion {
     const hostName = window.location.hostname.split('.')[0];
     let isNewReddit = false;
     let isOldReddit = false;
-    if (hostName === 'old') isOldReddit = true;
-    else if (hostName === 'new') isNewReddit = true;
 
-    // determine if new or old reddit
+    if (hostName === 'old') {
+        isOldReddit = true;
+    } else if (hostName === 'new') {
+        isNewReddit = true;
+    }
+
+    // Check for new Reddit by presence of shreddit-app
     if (!isNewReddit && !isOldReddit) {
         const shredditAppEls = document.getElementsByTagName('shreddit-app');
         if (shredditAppEls && shredditAppEls.length > 0) {
@@ -26,60 +46,101 @@ function main(): void {
         }
     }
 
+    return { isNewReddit, isOldReddit };
+}
+
+/**
+ * Main entry point functionality to execute once preferences are loaded.
+ * Determines if the current page is using old or new Reddit and attaches the necessary observers.
+ * @param preferences - The user's preferences for share button behavior.
+ */
+function main(preferences: UserPreferences): void {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { isNewReddit, isOldReddit } = isNewOrOldReddit();
+
     if (isOldReddit) {
-        const observer = new MutationObserver((mutationsList) => {
-            mutationsList.forEach((mutation) => {
-                mutation.addedNodes.forEach((element) => {
+        const observer = new MutationObserver((mutationsList: MutationRecord[]) => {
+            mutationsList.forEach((mutation: MutationRecord) => {
+                mutation.addedNodes.forEach((element: Node) => {
                     if (isElement(element) && element.classList.contains('post-sharing')) {
-                        const shareButtonEls = document.getElementsByClassName('post-sharing-option');
-                        if (shareButtonEls && shareButtonEls.length > 0) {
-                            const sibling = shareButtonEls[shareButtonEls.length - 1];
-                            addShareButton(sibling, 'right');
-                        }
+                        attachRecursiveObservers(element, preferences);
                     }
                 });
             });
         });
-        const observerParameters = {childList: true, subtree: true};
-        const topMatter = document.getElementsByClassName('top-matter')[0];
-        if (topMatter) {
-            observer.observe(topMatter, observerParameters);
-        } else {
-            const postEls = document.body.getElementsByClassName('entry unvoted');
-            for (const postEl of postEls)
-            {
-                observer.observe(postEl, observerParameters);
-            }
+        const observerParameters = { childList: true, subtree: true };
+        let appBody = document.querySelector('.content[role="main"]');
+        if (!appBody) {
+            appBody = document.body;
         }
-    } else if (isNewReddit) {
-        console.log('It\'s new reddit.');
+        observer.observe(appBody, observerParameters);
     }
 }
 
 /**
- * Adds the share button relative to its sibling. This is used in old reddit format
- * @param siblingElement the sibling element to add the share button to
- * @param position what position to add the share button to
+ * Recursively attaches mutation observers to the given element and its children.
+ * Observes for specific key elements (such as the share buttons or post-sharing containers),
+ * and disconnects when the button is found.
+ * @param element - The element to attach observers to.
+ * @param preferences - The user's preferences for the share button behavior.
  */
-function addShareButton(siblingElement: Element, position: 'right' | 'left'): void {
+function attachRecursiveObservers(element: Element, preferences: UserPreferences): void {
+    if (!isElement(element)) {
+        return;
+    }
+
+    if (element.classList.contains('post-sharing-option-embed')) {
+        addShareButton(element, 'right', preferences);
+        return;
+    }
+
+    const nestedObserver = new MutationObserver((nestedMutationsList: MutationRecord[]) => {
+        nestedMutationsList.forEach((nestedMutation: MutationRecord) => {
+            nestedMutation.addedNodes.forEach((nestedElement: Node) => {
+                if (isElement(nestedElement)) {
+                    attachRecursiveObservers(nestedElement, preferences);
+                }
+            });
+        });
+    });
+
+    nestedObserver.observe(element, {
+        childList: true,
+        subtree: true
+    });
+
+    const children = element.children;
+    for (let i = 0; i < children.length; i++) {
+        attachRecursiveObservers(children[i], preferences);
+    }
+}
+
+/**
+ * Adds the share button relative to its sibling. This is used in the old Reddit format.
+ * @param siblingElement - The sibling element to add the share button next to.
+ * @param position - The position to add the share button ('right' or 'left').
+ * @param preferences - The user's preferences for the share button behavior.
+ */
+function addShareButton(siblingElement: Element, position: 'right' | 'left', preferences: UserPreferences): void {
     const htmlString = '<div class="c-tooltip" role="tooltip">' +
         '<div class="tooltip-arrow bottom"></div>' +
         '<div class="tooltip-inner">Better Embed Link</div></div>';
     const shareButtonDiv = document.createElement('div');
     shareButtonDiv.innerHTML = htmlString;
     shareButtonDiv.className = 'post-sharing-option better-share-button';
-    shareButtonDiv.addEventListener('click', shareButtonClick);
+    shareButtonDiv.addEventListener('click', () => {
+        shareButtonClick(preferences);
+    });
     if (position === 'right') {
         siblingElement.insertAdjacentElement('afterend', shareButtonDiv);
-        console.log(shareButtonDiv);
     } else if (position === 'left') {
         siblingElement.parentElement?.insertBefore(shareButtonDiv, siblingElement.firstChild);
     }
 }
 
 /**
- * Gets the URL of the reddit post
- * @returns the URL of the reddit post
+ * Gets the URL of the Reddit post, either from the input field or the window location.
+ * @returns The URL of the Reddit post.
  */
 export function getPostURL(): string {
     const linkInputEl = document.body.getElementsByClassName('post-sharing-link-input')[0];
@@ -89,17 +150,40 @@ export function getPostURL(): string {
         url = url?.split('?')[0];
     }
     if (!url) url = window.location.toString();
-    // TODO: get the URL from the input text box
     url = url.replace('old.reddit.', 'reddit.');
     url = url.replace('new.reddit.', 'reddit.');
-    return url.replace('reddit.', 'vxreddit.');
+    return url;
 }
 
 /**
- * The behavior for when the custom share button is clicked
+ * Converts the given Reddit URL to a shareable URL based on user preferences.
+ * @param url - The original Reddit URL.
+ * @param preferences - The user's preferences for converting the URL.
+ * @returns The converted, shareable URL.
  */
-function shareButtonClick() {
-    const url = getPostURL();
+export function convertToShareableURL(url: string, preferences: UserPreferences): string {
+    let newURL: string;
+    switch (preferences.reddit) {
+    case "rxddit":
+        newURL = url.replace('reddit.', 'rxddit.');
+        break;
+    case "vxreddit":
+        newURL = url.replace('reddit.', 'vxreddit.');
+        break;
+    default:
+        newURL = url;
+    }
+    return newURL;
+}
+
+/**
+ * Handles the behavior when the custom share button is clicked.
+ * Copies the converted shareable URL to the clipboard.
+ * @param preferences - The user's preferences for converting and sharing the URL.
+ */
+function shareButtonClick(preferences: UserPreferences): void {
+    let url = getPostURL();
+    url = convertToShareableURL(url, preferences);
     navigator.clipboard.writeText(url).then(() => {
         console.log('Copied ' + url + ' to clipboard!');
     });
---
+++ src/settings.ts
@@ -0,0 +1,73 @@
+﻿export type RedditPreference = 'rxddit' | 'vxreddit';
+export type XPreference = 'fixupx';
+export type TwitterPreference = 'fxtwitter' | 'twittpr' | 'vxtwitter';
+export type InstagramPreference = 'ddinstagram';
+type PreferenceOverride<T> = 'default' | T;
+
+interface BSBPreferences { 
+        version: string;
+}
+
+interface BSBPreferences_v1 extends BSBPreferences {
+    version: '1',
+    reddit: RedditPreference,
+    x: XPreference,
+    twitter: TwitterPreference,
+    instagram: InstagramPreference,
+}
+
+export type UserPreferences = BSBPreferences_v1;
+const CURRENT_VERSION = '1';
+
+/**
+ * Determines if the object is a preferences file of the current version
+ * @param preferences The object we're checking
+ * @returns true if the object is a Preferences object of the current version
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function isPreferencesCurrentVersion(preferences: any): preferences is UserPreferences
+{
+    return (preferences && preferences.version && preferences.version === CURRENT_VERSION);
+}
+
+/**
+ * Creates a new preferences object from the defaults
+ * @returns a preferences object from defaults
+ */
+function defaultPreferences(): UserPreferences {
+    return {
+        version: '1',
+        reddit: 'vxreddit',
+        x: 'fixupx',
+        twitter: 'fxtwitter',
+        instagram: 'ddinstagram',
+    };
+}
+
+let preferences: UserPreferences | null;
+
+/**
+ * Loads the preferences
+ * @returns a Promise containing the preferences
+ */
+export async function loadPreferences(): Promise<UserPreferences> {
+    if (preferences) return preferences;
+    const results = await browser.storage.local.get('preferences');
+    if (results.preferences) {
+        preferences = results.preferences;
+        if (isPreferencesCurrentVersion(preferences)) {
+            return preferences;
+        } 
+    }
+    preferences = defaultPreferences();
+    await savePreferences();
+    return defaultPreferences();
+}
+
+/**
+ * Saves the current preferences
+ */
+export async function savePreferences(): Promise<void> {
+    if (!preferences) return;
+    await browser.storage.local.set({preferences: preferences});
+}
