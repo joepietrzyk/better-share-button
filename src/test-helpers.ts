f260ff90603851b0a04104e8b878dde274a84f5f
Joe Pietrzyk <joepietrzyk@gmail.com>
2024-09-26 10:50:19 -0400

    Split shared test code into its own file

---
+++ src/test-helpers.ts
@@ -0,0 +1,22 @@
+﻿import {JSDOM} from 'jsdom';
+
+/**
+ * Uses JSDOM to stub the global document with the provided HTML
+ * @param htmlString - a string containing the HTML of the HTML document to stub
+ */
+export function setHTMLStringAsDocument(htmlString: string) {
+  const dom = new JSDOM(htmlString);
+  vi.stubGlobal('window', dom.window);
+  vi.stubGlobal('document', dom.window.document);
+}
+
+/**
+ * Returns a `Promise` that resolves on the next frame
+ */
+export function resolveOnNextFrame(): Promise<void> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 0);
+  });
+}
