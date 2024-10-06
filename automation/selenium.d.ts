6b177fbbaef7f5fe69479406c41c80673a2296ec
Joe Pietrzyk <joepietrzyk@gmail.com>
2024-10-05 21:05:56 -0400

    Better comments

--- automation/selenium.d.ts
+++ automation/selenium.d.ts
@@ -3,9 +3,31 @@ import { WebElement } from 'selenium-webdriver';
 
 declare module 'selenium-webdriver' {
   interface WebDriver {
-    getAndWait(url: string): Promise<void>;
+    /**
+     * Gets the URL and waits the specified amount of time for the page to load
+     * @param url - the URL to get
+     * @param ms - the ms to wait
+     * @returns a `Promise` that resolves when the page is loaded, and the specified ms have been waited
+     */
+    getAndWait(url: string, ms: number = 50): Promise<void>;
+
+    /**
+     * Gets the clipboard text
+     * @returns a `Promise` that resolves with the clipboard content
+     */
     getClipboardText(): Promise<string>;
+
+    /**
+     * Closes out the browser for test cases, respecting `process.env.LEAVE_OPEN`
+     * @returns a `Promise` that resolves when the `WebDriver` is closed
+     */
     closeTest(): Promise<void>;
+
+    /**
+     * Scrolls the element into view
+     * @param element the element to scroll into view
+     * @returns a `Promise` that resolves when the element is in view
+     */
     scrollElementIntoView(element: WebElement): Promise<void>;
   }
 }
